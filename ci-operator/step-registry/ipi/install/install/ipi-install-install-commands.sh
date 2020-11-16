d42ef676e6c0456933f36e8e051d7f6302418af5
Danielle Barda <dbarda@redhat.com>
2020-11-16 11:39:25 +0200

    In cluster-type kubevirt copy kubeconfig to be under home directory

--- ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
+++ ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
@@ -21,7 +21,9 @@ case "${CLUSTER_TYPE}" in
 aws) export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred;;
 azure4) export AZURE_AUTH_LOCATION=${CLUSTER_PROFILE_DIR}/osServicePrincipal.json;;
 gcp) export GOOGLE_CLOUD_KEYFILE_JSON=${CLUSTER_PROFILE_DIR}/gce.json;;
-kubevirt) export KUBEVIRT_KUBECONFIG=${HOME}/.kube/config;;
+kubevirt)
+  mkdir -p ${HOME}/.kube/
+  cp ${CLUSTER_PROFILE_DIR}/kubeconfig  ${HOME}/.kube/config;;
 vsphere) ;;
 openstack) export OS_CLIENT_CONFIG_FILE=${CLUSTER_PROFILE_DIR}/clouds.yaml ;;
 openstack-vexxhost) export OS_CLIENT_CONFIG_FILE=${CLUSTER_PROFILE_DIR}/clouds.yaml ;;
